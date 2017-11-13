e0a9076b81852a52be56d9e96a38dfdb7ba86697
Shea Lignitz <rlignitz@gmail.com>
2017-11-13 22:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var customerName = 'bob';
 function upperCaseCustomerName(lowercaseName) {
   return customerName = customerName.toUpperCase();
 }
+
+function setBestCustomer() {
+  var bestCustomer = 'not bob';
+}
